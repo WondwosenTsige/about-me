88b66f1f1ef1b11fa083fbf2f080c527d02ef307
Wondwosen Tsige <wondneh@gmail.com>
2020-10-27 17:41:13 -0700

    about-me guessing js file update ACP

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 'use strict'
+
+var userName = prompt('What is your name please?');
+alert('Welcome to my site ' + userName);
+
+    var workStatus = prompt('Are you currently working?');
+
+    if(workStatus.toLowerCase() === 'yes'){
+        alert('I am glad your job is not affected by Covid-19');
+    } else {
+        alert('Me too '+userName+ ' . I hope we will be software developers soon');
+    }
+
+        var takeVacation = prompt('Do you think I will go some where for vacation b/n code-301 and code-401?')
+
+        if(takeVacation.toLowerCase() === 'yes'){
+            alert('Nice guess! I am planning to');
+        } else {
+            alert('No! you get this wrong. Let us proceed to my next question');
+        }
+
+            var swimAbility =prompt('Can you swim?');
+
+            if(swimAbility.toUpperCase() === 'YES'){
+                alert('Great! may be you will teach me in the future');
+            } else {
+                alert('Oh! Just like me. I am thinking to learn swimming soon');
+            }
+
+        var swimAbility =prompt('Can you swim?');
+
+        if(swimAbility.toUpperCase() === 'YES'){
+            alert('Great! may be you will teach me in the future');
+        } else {
+            alert('Oh! Just like me. I am thinking to learn swimming soon');
+        }
+
+    var myAge= prompt('How old do you think i am ' +userName+'?')
+
+    if(myAge == 40){
+        alert ('Yes! you get it right ' +userName+ ' See you next time.');
+    } else {
+        alert ('No I am 40 ' +userName+ ' See you next time.');
+    }
+
+
