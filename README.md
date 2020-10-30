ada200e25bab7d1cbf3d7eae181ab846f91b8f92
Wondwosen Tsige <wondneh@gmail.com>
2020-10-29 20:31:23 -0700

    labs 3 and 4 completed

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # This is a guessing game about me project for the fullfilment of code201
 
 - Chance, one of the lab instructors helped me in the number guessing game in using the "break;" command and "parseInt" method. credit to him
-- I will definetly update this read me after reading good practices to follow
+- William Moreno also contributed in this guessing me as a partner in pair programming 
+- The guessing game with the array tough for me and it works now with the help of today's( Oct 29 2020) code review by playing back and refering the FrontRow video
--- js/app.js
+++ js/app.js
@@ -1,62 +1,61 @@
 'use strict'
-
 var userName = prompt('What is your name please?');
 alert('Welcome to my site ' + userName);
 
 var rightAnswerCounter = 0;
 
-function firstQuestion(){
+function areYouWorking() {
     var workStatus = prompt('Are you currently working?');
 
-    if(workStatus.toLowerCase() === 'yes'){
+    if (workStatus.toLowerCase() === 'yes') {
         //console.log('I am glad your job is not affected by Covid-19');
         alert('I am glad your job is not affected by Covid-19');
         rightAnswerCounter++;
-    } else if (workStatus.toLowerCase()=== 'no'){
+    } else if (workStatus.toLowerCase() === 'no') {
         //console.log('Me too '+userName+ ' . I hope we will be software developers soon');
-        alert('Me too '+userName+ ' . I hope we will be software developers soon');
+        alert('Me too ' + userName + ' . I hope we will be software developers soon');
     } else {
         //console.log ('Please answer yes or no')
-        alert ('Please answer yes or no')
+        alert('Please answer yes or no')
     }
 }
 
-function vacation(){
+function vacation() {
     var takeVacation = prompt('Do you think I will go some where for vacation b/n code-301 and code-401?')
 
-    if(takeVacation.toLowerCase() === 'yes'){
+    if (takeVacation.toLowerCase() === 'yes') {
         //console.log('Nice guess! I am planning to');
         alert('Nice guess! I am planning to');
         rightAnswerCounter++;
 
-    } else if (takeVacation.toLowerCase()==='no') {
+    } else if (takeVacation.toLowerCase() === 'no') {
         //console.log('No! you get this wrong. Let us proceed to my next question');
         alert('No! you get this wrong. Let us proceed to my next question');
     } else {
         //console.log('Please answer yes or no')
-        alert ('Please answer yes or no')
+        alert('Please answer yes or no')
     }
 }
-function youSwim(){
-    var swimAbility =prompt('Can you swim?');
+function youSwim() {
+    var swimAbility = prompt('Can you swim?');
 
-    if(swimAbility.toUpperCase() === 'YES'){
+    if (swimAbility.toUpperCase() === 'YES') {
         //console.log('Great! may be you will teach me in the future');
         alert('Great! may be you will teach me in the future');
         rightAnswerCounter++;
 
-    } else if (swimAbility.toUpperCase() === 'NO'){
+    } else if (swimAbility.toUpperCase() === 'NO') {
         //console.log('Oh! Just like me. I am thinking to learn swimming soon');
         alert('Oh! Just like me. I am thinking to learn swimming soon');
     } else {
         //console.log ('Please answer yes or no')
-        alert ('Please answer yes or no')
+        alert('Please answer yes or no')
     }
 }
-function footballFan(){
-    var LikeFootball =prompt('Do you think that I like football?');
+function footballFan() {
+    var LikeFootball = prompt('Do you think that I like football?');
 
-    if(LikeFootball.toUpperCase() === 'YES'){
+    if (LikeFootball.toUpperCase() === 'YES') {
         //console.log('Good job! I am a die hard Liverpool fun');
         alert('Good job! I am a die hard Liverpool fun');
         rightAnswerCounter++;
@@ -66,83 +65,86 @@ function footballFan(){
         alert('No no no!!!! Liverpool is my team and "You will never walk alone" is our anthem');
     } else {
         //console.log('Please answer yes or no')
-        alert ('Please answer yes or no')
+        alert('Please answer yes or no')
     }
 }
-function rateMyGame(){
-    var rateGame= prompt('Do you like our game so far ' +userName+'?')
+function rateMyGame() {
+    var rateGame = prompt('Do you like our game so far ' + userName + '?')
 
-    if(rateGame.toUpperCase() === 'YES'){
+    if (rateGame.toUpperCase() === 'YES') {
         //console.log('Thank you '+userName+ 'see you next time');
-        alert('Thank you '+userName+ ' see you next time');
+        alert('Thank you ' + userName + ' see you next time');
         rightAnswerCounter++;
 
     } else if (rateGame.toUpperCase() === 'NO') {
         //console.log('Sorry '+userName+ 'I will try to make it better next time. see you!');
-        alert('Sorry '+userName+ 'I will try to make it better next time.');
+        alert('Sorry ' + userName + 'I will try to make it better next time.');
     } else {
         //console.log ('Please answer yes or no')
-        alert ('It was a yes or no question. bye for now')
+        alert('It was a yes or no question. bye for now')
     }
 }
 
-firstQuestion();
-vacation();
-youSwim();
-footballFan();
-rateMyGame();
-
-function myFavoriteNumber(){
+function myFavoriteNumber() {
     var favoriteNumber = 7;
 
-    for (var i=0; i<6; i++){
-        var userGuess= parseInt(prompt('Please guess my favorite number?'));
+    for (var i = 0; i < 6; i++) {
+        var userGuess = parseInt(prompt('Please guess my favorite number?'));
 
-        if(userGuess === favoriteNumber){
-            alert('Wow! you got it right '+userName+ ' One more qestion left');
-            break;
+        if (userGuess === favoriteNumber) {
+            alert('Wow! you got it right ' + userName + ' One more qestion left');
             rightAnswerCounter++;
+            break;
 
-        } else if (i===5 &&(userGuess) !== favoriteNumber){
-            alert ('you are out of guesses favorite number was '+ favoriteNumber);
+        } else if (i === 5 && (userGuess) !== favoriteNumber) {
+            alert('you are out of guesses favorite number was ' + favoriteNumber);
 
         } else if (userGuess > favoriteNumber) {
             //console.log('Sorry '+userName+ 'I will try to make it better next time. see you!');
-            alert('Sorry '+userName+ 'your guess is higher. Try to guess a lower nymber');
+            alert('Sorry ' + userName + 'your guess is higher. Try to guess a lower nymber');
 
-        }  else if(userGuess < favoriteNumber){
+        } else if (userGuess < favoriteNumber) {
             //console.log ('Please answer yes or no')
-            alert ('your guess is lower. Try to guess a higher nymber');
+            alert('your guess is lower. Try to guess a higher nymber');
 
         } else {
-            alert ('try again');
+            alert('try again');
         }
 
     }
+
 }
-myFavoriteNumber();
 
-// var favoritePlacesToVisit = ['Colosseum', 'Rock-Hewn Churches of Lalibela', 'Eiffel Tower', 'Taj Mahal', 'Alcatraz Island', 'The Obelisk of Axum'];
-// var guessPlacesToVisit = prompt ('can you guess one of the places I really want to visit?');
-// // var numberOfGuess = 0;
 
-// for( var i = 0 ; i < 6 ; i++ ) {
-//     for( var j = 0 ; j < favoritePlacesToVisit.length ; j++ ) {
+function myFavoritePlace() {
+    var favoritePlacesToVisit = ['Colosseum', 'Lalibela', 'Eiffel Tower', 'Taj Mahal', 'Alcatraz', 'Axum'];
+    var guessPlacesToVisit = prompt('can you guess one of the places I really want to visit?');
+    var rightAnswer = false;
 
-//     if(guessPlacesToVisit === favoritePlacesToVisit[j]){
-//         alert ('You got it right! the whole  lists of places I want to visit are '+ favoritePlacesToVisit);
-//         rightAnswerCounter++;
-//         i = 6;
-//         break;
-//     }else if( i === 5 ){
-//         guessPlacesToVisit = prompt ('No! '+ favoritePlacesToVisit+ 'is the list of places I want to visit');
-//         break;
-//     }else if( i === 4 ){
-//         guessPlacesToVisit = prompt ('Sorry. That\'s wrong ' + userName + '. You only have one guess left.');
-//     }else {
-//         guessPlacesToVisit = prompt ('Wrong. Try again.');
-//     }
+    for (var i = 0; i < 6; i++) {                                    //This is the start of the outer loop to count number of user's guess.
+        for (var j = 0; j < favoritePlacesToVisit.length; j++) {     //This inner loop loops in to the favoritesPlacesToVisit array
 
-//     // alert ('You got '+rightAnswerCounter+ ' right answers. Thank you '+userName+ '! See you next time');
-//     }
-// }
+            if (guessPlacesToVisit.toLowerCase() === favoritePlacesToVisit[j]) {
+                alert('You got it right! the whole  lists of places I want to visit are ' + favoritePlacesToVisit);
+                
+                i = 6;
+                rightAnswer = true;
+                rightAnswerCounter++;
+                break;
+            }
+        }
+        if (rightAnswer === false) {
+            alert('Wrong')
+            guessPlacesToVisit = prompt('Sorry you are wrong. Please try again');
+        }
+    }
+}
+    areYouWorking();
+    vacation();
+    youSwim();
+    footballFan();
+    rateMyGame();
+    myFavoriteNumber();
+    myFavoritePlace();
+
+
